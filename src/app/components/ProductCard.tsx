e5012d91d26eba5fd5d1209cf7cb25dcfc02ed8d
Jasen Peterson <me@jasenmp.com>
2024-10-16 07:23:24 -0500

    Basic layout.

---
+++ src/app/components/ProductCard.tsx
@@ -0,0 +1,16 @@
+import { ProductInterface } from "@/app/api/types";
+import Card from "@/app/components/Card";
+
+interface ProductCardProps {
+  product: ProductInterface;
+}
+
+export default function ProductCard({ product }: ProductCardProps) {
+  return (
+    <Card>
+      <h2 className="text-lg font-semibold">{product.title}</h2>
+      <p className="text-gray-500">{product.description}</p>
+      <p className="text-gray-700">${product.price}</p>
+    </Card>
+  );
+}
--- src/app/components/ProductsGrid.tsx
+++ src/app/components/ProductsGrid.tsx
@@ -1,18 +1,8 @@
 import fetchData from "@/app/api/requests";
 import Grid from "@/app/components/Grid";
-import Card from "@/app/components/Card";
+import ProductCard from "@/app/components/ProductCard";
 import NotFound from "@/app/components/NotFound";
 import { ProductInterface } from "@/app/api/types";
-
-function ProductCard({ product }: { product: ProductInterface }) {
-  return (
-    <Card>
-      <h2 className="text-lg font-semibold">{product.title}</h2>
-      <p className="text-gray-500">{product.description}</p>
-      <p className="text-gray-700">${product.price}</p>
-    </Card>
-  );
-}
 
 export default async function ProductsGrid() {
   const products: ProductInterface[] = await fetchData();
