8e2681c3c27ad6ad930a85bb1a0d381e7559f9e0
Jasen Peterson <me@jasenmp.com>
2024-10-18 06:49:36 -0500

    Refactoring requests.ts

--- src/app/api/requests.ts
+++ src/app/api/requests.ts
@@ -1,8 +1,22 @@
-export default async function fetchData(endpoint?: string, method?: string) {
-  const url = process.env.API_URL! + (endpoint || "");
-  const response = await fetch(url, {
+import { Product } from "./types";
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+export async function fetchProducts() {
+  const response = await fetch(process.env.API_URL!, {
     cache: "no-store",
-    method: method || "GET",
+    headers,
   });
-  return await response.json();
+  return (await response.json()) as Product[];
+}
+
+export async function createProduct(body: Product) {
+  const response = await fetch(`${process.env.API_URL}/create-product`, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers,
+  });
+  return (await response.json()) as Product;
 }
