9f92c2f508bc6dc64d2e6f92b9c105aee4236444
Jasen Peterson <me@jasenmp.com>
2024-10-18 06:10:10 -0500

    Implementing zod.

--- src/app/api/types.ts
+++ src/app/api/types.ts
@@ -1,16 +1,20 @@
-export interface ImageInterface {
-  url: string;
-  isPrimary?: boolean;
-  _id?: string;
-}
+import { z } from "zod";
 
-export interface ProductInterface {
-  _id?: string;
-  title: string;
-  description: string;
-  price: number;
-  category_ids: string[];
-  images: ImageInterface[];
-  discount?: number;
-  isPublished: boolean;
-}
+const ImageSchema = z.object({
+  url: z.string(),
+  isPrimary: z.boolean().optional(),
+  _id: z.string().optional(),
+});
+
+const ProductSchema = z.object({
+  _id: z.string().optional(),
+  title: z.string(),
+  description: z.string(),
+  price: z.number(),
+  category_ids: z.array(z.string()),
+  images: ImageSchema.array(),
+  discount: z.number().optional(),
+  isPublished: z.boolean(),
+});
+
+export type ProductSchema = z.infer<typeof ProductSchema>;
