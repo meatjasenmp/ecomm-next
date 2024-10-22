b39c8e63afbe5edf860937ecba7e4e1084a511b8
Jasen Peterson <me@jasenmp.com>
2024-10-22 07:29:25 -0500

    Init product form.

--- src/app/components/ProductForm/styles.ts
+++ src/app/components/ProductForm/styles.ts
@@ -1,16 +1,14 @@
 export const containerStyles = {
   display: "flex",
-  appearance: "rounded-md shadow-sm ring-1 ring-inset ring-gray-300",
+  appearance: "ring-gray-300 ring-1 ring-inset rounded-md shadow-sm",
   size: "sm:max-w-md",
-  focus:
-    "focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600",
 };
 
 export const inputStyles = {
   display: "block",
   size: "w-full",
   spacing: "py-1.5",
-  appearance: "rounded-md border-0 ring-1 ring-inset ring-gray-300 shadow-sm",
+  appearance: `${containerStyles.appearance}, rounded-md border-0`,
   focus: "focus:ring-2 focus:ring-inset focus:ring-indigo-600",
   font: "sm:text-sm sm:leading-6 text-gray-900",
   placeholder: "placeholder:text-gray-400",
