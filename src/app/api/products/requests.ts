67800e1df0b3e6988eba4135d07fa822f77836ab
Jasen Peterson <me@jasenmp.com>
2024-10-28 07:04:48 -0500

    Removing const headers.

--- src/app/api/products/requests.ts
+++ src/app/api/products/requests.ts
@@ -1,13 +1,8 @@
 import { Product } from "../types";
 
-const headers = {
-  "Content-Type": "application/json",
-};
-
 export async function fetchProducts() {
   const response = await fetch(process.env.API_URL!, {
     cache: "no-store",
-    headers,
   });
   return (await response.json()) as Product[];
 }
@@ -16,7 +11,6 @@ export async function createProduct(body: Product) {
   const response = await fetch(`${process.env.API_URL}/create-product`, {
     method: "POST",
     body: JSON.stringify(body),
-    headers,
   });
   return (await response.json()) as Product;
 }
