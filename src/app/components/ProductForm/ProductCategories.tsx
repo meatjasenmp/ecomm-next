d54ad112ce8923ee7b458561cc7ad4eaea9f14d7
Jasen Peterson <me@jasenmp.com>
2024-10-31 07:20:00 -0500

    Init product categories request.

--- src/app/components/ProductForm/ProductCategories.tsx
+++ src/app/components/ProductForm/ProductCategories.tsx
@@ -11,6 +11,17 @@ import {
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import { Category } from "@/app/api/categories/types";
 
+import { parseStyles } from "@/app/helpers/styles";
+import {
+  labelStyles,
+  listBoxButtonStyles,
+  listBoxCheckIcon,
+  listBoxChevronContainer,
+  listBoxOptionsStyles,
+  listBoxOptionStyles,
+  listBoxOptionValueStyles,
+} from "@/app/components/ProductForm/styles";
+
 interface ProductCategoriesProps {
   categories: Category[];
 }
@@ -22,13 +33,11 @@ export default function ProductCategories({
 
   return (
     <Listbox value={selectedCategory} onChange={setSelectedCategory}>
-      <Label className="block text-sm/6 font-medium text-gray-900">
-        Assigned to
-      </Label>
+      <Label className={parseStyles(labelStyles)}>Assigned to</Label>
       <div className="relative mt-2">
-        <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm/6">
+        <ListboxButton className={parseStyles(listBoxButtonStyles)}>
           <span className="block truncate">{selectedCategory}</span>
-          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+          <span className={parseStyles(listBoxChevronContainer)}>
             <ChevronUpDownIcon
               aria-hidden="true"
               className="h-5 w-5 text-gray-400"
@@ -38,19 +47,19 @@ export default function ProductCategories({
 
         <ListboxOptions
           transition
-          className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
+          className={parseStyles(listBoxOptionsStyles)}
         >
           {categories.map((category) => (
             <ListboxOption
               key={category._id}
               value={category.name}
-              className="group relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+              className={parseStyles(listBoxOptionStyles)}
             >
-              <span className="block truncate font-normal group-data-[selected]:font-semibold">
+              <span className={parseStyles(listBoxOptionValueStyles)}>
                 {category.name}
               </span>
 
-              <span className="absolute inset-y-0 left-0 flex items-center pl-1.5 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+              <span className={parseStyles(listBoxCheckIcon)}>
                 <CheckIcon aria-hidden="true" className="h-5 w-5" />
               </span>
             </ListboxOption>
--- src/app/components/ProductForm/styles.ts
+++ src/app/components/ProductForm/styles.ts
@@ -21,3 +21,61 @@ export const leadingAddOns = {
   appearance: "inset-y-0",
   span: "text-gray-500 sm:text-sm",
 };
+
+export const listBoxButtonStyles = {
+  size: inputStyles.size,
+  position: "relative",
+  appearance: inputStyles.appearance,
+  cursor: "cursor-default",
+  spacing: "py-1.5 pl-3 pr-10",
+  font: `text-gray-900 sm:text-sm/6 text-left`,
+  focus:
+    "focus:ring-2 focus:ring-inset focus:ring-indigo-600 focus:outline-none",
+  height: "max-h-60",
+};
+
+export const listBoxOptionsStyles = {
+  size: inputStyles.size,
+  position: "absolute",
+  zIndex: "z-10",
+  spacing: "mt-1",
+  height: "max-h-60",
+  overflow: "overflow-auto",
+  appearance: "rounded-md shadow-lg ring-1 ring-black ring-opacity-5",
+  background: "bg-white",
+  font: "text-base sm:text-sm",
+  focus: "focus:outline-none",
+  dataClosed: "data-[closed]:data-[leave]:opacity-0",
+  dataLeave:
+    "data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in",
+};
+
+export const listBoxOptionStyles = {
+  position: "group relative",
+  cursor: "cursor-default",
+  select: "select-none",
+  spacing: "py-2 pl-8 pr-4",
+  dataFocus: "data-[focus]:bg-indigo-600 data-[focus]:text-white",
+};
+
+export const listBoxOptionValueStyles = {
+  display: "block",
+  font: "font-normal truncate",
+  groupData: "group-data-[selected]:font-semibold",
+};
+
+export const listBoxCheckIcon = {
+  position: "absolute inset-y-0 left-0  pl-1.5",
+  font: "text-indigo-600",
+  spacing: "pl-1.5",
+  display: "flex items-center",
+  groupData: "group-data-[focus]:text-white",
+  groupNotData: "[.group:not([data-selected])_&]:hidden",
+};
+
+export const listBoxChevronContainer = {
+  position: "absolute inset-y-0 right-0",
+  display: "flex items-center",
+  spacing: "pr-2",
+  pointerEvents: "pointer-events-none",
+};
