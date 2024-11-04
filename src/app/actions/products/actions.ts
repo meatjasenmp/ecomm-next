9dc3fce2590dd85814a40f0f3c6d5b62b0c28d4e
Jasen Peterson <me@jasenmp.com>
2024-11-04 06:12:37 -0600

    WIP. Hooking up form.

--- src/app/actions/products/actions.ts
+++ src/app/actions/products/actions.ts
@@ -1,6 +1,8 @@
 "use server";
+
 import { createProduct } from "@/app/api/products/requests";
 import { ProductSchema, Product } from "@/app/api/products/types";
+import { revalidatePath } from "next/cache";
 
 export async function addProduct(
   prevState: {
@@ -25,6 +27,7 @@ export async function addProduct(
 
   try {
     await createProduct(data);
+    revalidatePath("/admin");
     return { message: "Product created successfully" };
   } catch (err) {
     return { message: "Failed to create Product", error: err };
