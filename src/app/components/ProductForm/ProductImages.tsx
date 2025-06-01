5413525e56e14ed6adee2d9d3a0e3220192c2a50
Jasen Peterson <me@jasenmp.com>
2025-06-01 10:15:26 -0500

    Basic Product image styles.

--- src/app/components/ProductForm/ProductImages.tsx
+++ src/app/components/ProductForm/ProductImages.tsx
@@ -1,19 +1,20 @@
+import { Field } from "@base-ui-components/react/field";
+import { PhotoIcon } from "@heroicons/react/20/solid";
+import styles from "@/app/styles/components/form.module.css";
+import images from "@/app/styles/components/productImages.module.css";
+
 export default function ProductImages() {
   return (
-    <section>
-      <label htmlFor="cover-photo">Cover photo</label>
-      <div>
+    <Field.Root className={styles.fieldroot}>
+      <Field.Label htmlFor="images" className={styles.fieldlabel}>
+        Product Images
+      </Field.Label>
+      <div className={images.container}>
+        <PhotoIcon className={images.icon} />
         <div>
-          <div>
-            <label htmlFor="images">
-              <span>Upload a file</span>
-              <input id="images" name="images" type="file" />
-            </label>
-            <p>or drag and drop</p>
-          </div>
-          <p>PNG, JPG, GIF up to 10MB</p>
+          <input id="images" name="images" type="file" />
         </div>
       </div>
-    </section>
+    </Field.Root>
   );
 }
