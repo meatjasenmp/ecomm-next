d4da9585bd300188d8859db960f5c796b768a9b8
Jasen Peterson <me@jasenmp.com>
2025-06-01 10:21:10 -0500

    Product price & discount

--- src/app/components/ProductForm/ProductDiscount.tsx
+++ src/app/components/ProductForm/ProductDiscount.tsx
@@ -1,14 +1,18 @@
 import { Field } from "@base-ui-components/react/field";
+import styles from "@/app/styles/components/form.module.css";
 
 export default function ProductDiscount() {
   return (
-    <Field.Root>
-      <Field.Label htmlFor="price">Product Discount</Field.Label>
+    <Field.Root className={styles.fieldroot}>
+      <Field.Label htmlFor="discount" className={styles.fieldlabel}>
+        Product Discount
+      </Field.Label>
       <Field.Control
         id="discount"
         name="discount"
         type="text"
         placeholder="0.00"
+        className={styles.fieldcontrol}
       />
     </Field.Root>
   );
--- src/app/components/ProductForm/ProductPrice.tsx
+++ src/app/components/ProductForm/ProductPrice.tsx
@@ -1,15 +1,19 @@
 import { Field } from "@base-ui-components/react/field";
+import styles from "@/app/styles/components/form.module.css";
 
 export default function ProductPrice() {
   return (
-    <Field.Root>
-      <Field.Label htmlFor="price">Price</Field.Label>
+    <Field.Root className={styles.fieldroot}>
+      <Field.Label htmlFor="price" className={styles.fieldlabel}>
+        Price
+      </Field.Label>
       <Field.Control
         required
         id="price"
         name="price"
         type="text"
         placeholder="0.00"
+        className={styles.fieldcontrol}
       />
     </Field.Root>
   );
