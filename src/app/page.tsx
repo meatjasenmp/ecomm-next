5c7c4f9fca9a8511c199512d26458f08b79af2d4
Jasen Peterson <me@jasenmp.com>
2025-06-03 07:30:37 -0500

    Removing unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Container from "@/app/parts/Container";
-import Sidebar from "@/app/parts/Sidebar";
 import ProductsGrid from "@/app/components/ProductsGrid";
 
 export default async function Page() {
