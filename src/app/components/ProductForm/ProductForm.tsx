bc1d9f767e0161a368e4185f516c896fc0a2e408
Jasen Peterson <me@jasenmp.com>
2025-06-06 07:43:09 -0500

    Removing import

--- src/app/components/ProductForm/ProductForm.tsx
+++ src/app/components/ProductForm/ProductForm.tsx
@@ -4,7 +4,6 @@ import Form from "next/form";
 import { useActionState } from "react";
 import { Fieldset } from "@base-ui-components/react/fieldset";
 import { addProduct } from "@/app/actions/products/actions";
-import ProductFormHeader from "@/app/components/ProductForm/ProductFormHeader";
 import ProductFormFooter from "@/app/components/ProductForm/ProductFormFooter";
 import { CategoriesProps } from "@/app/admin/products/create/page";
 import styles from "@/app/styles/components/form.module.css";
